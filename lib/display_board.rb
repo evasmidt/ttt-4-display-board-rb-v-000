6cef186a47a24d0d87d70f08c2da136a09aa85ba
Eva Smidt <shevangeline@gmail.com>
2018-01-25 02:45:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,6 @@
 
 def display_board(board)
   puts "#{board[0]}"
-  puts "#{board[1]}"
-  puts "#{board[2]}"
-  puts "#{board[3]}"
-  puts "#{board[4]}"
 end
 
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
